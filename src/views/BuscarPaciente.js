eaf68b3237fd289bbc02f4aec3756e711fb3fef9
MgkJosue <masderxd@gmail.com>
2023-07-05 10:17:07 -0500

    Paginacion y busqueda por nombre-apllido y cedula

--- src/views/BuscarPaciente.js
+++ src/views/BuscarPaciente.js
@@ -3,78 +3,116 @@ import axios from 'axios';
 import '../styles/BuscarPaciente.css';
 
 export default function BuscarPaciente({ onViewChange }) {
-
-  const handleViewChange = (view) => {
-    onViewChange(view);
+  const handleViewChange = (view, pacienteId) => {
+    onViewChange(view, pacienteId);
   };
 
   const [formData, setFormData] = useState({
     search: '',
+    patientsPerPage: 10,
   });
 
   const [pacientes, setPacientes] = useState([]);
   const [filteredPacientes, setFilteredPacientes] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const fetchPacientes = async () => {
+    try {
+      const response = await axios.get('http://localhost:8000/pacientes/');
+      setPacientes(response.data);
+      setFilteredPacientes(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   useEffect(() => {
-    const fetchPacientes = async () => {
-      try {
-        const response = await axios.get(`http://localhost:8000/pacientes`);
-        setPacientes(response.data);
-        setFilteredPacientes(response.data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
     fetchPacientes();
   }, []);
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
+    const searchValue = e.target.value.toLowerCase();
     const filtered = pacientes.filter((paciente) => {
-      return (paciente.cedula && paciente.cedula.includes(formData.search)) || 
-             (paciente.nombre && paciente.nombre.includes(formData.search));
+      const fullName = `${paciente.Nombre} ${paciente.Apellido}`.toLowerCase();
+      return fullName.includes(searchValue) || paciente.Cedula.includes(searchValue);
     });
     setFilteredPacientes(filtered);
+    setCurrentPage(1);
+  };
+
+  const handlePatientsPerPageChange = (e) => {
+    setFormData({ ...formData, patientsPerPage: parseInt(e.target.value) });
+    setCurrentPage(1);
+  };
+
+  const indexOfLastPatient = currentPage * formData.patientsPerPage;
+  const indexOfFirstPatient = indexOfLastPatient - formData.patientsPerPage;
+  const currentPatients = filteredPacientes.slice(indexOfFirstPatient, indexOfLastPatient);
+
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
   };
 
   return (
-    <>
-    <form onSubmit={handleSubmit} className="buscarPaciente-form">
-      <h2>Buscar Paciente</h2>
-      <div className="input-group">
-        <label htmlFor="search">Buscar por cédula o nombre</label>
-        <input type="text" name="search" id="search" onChange={handleChange} value={formData.search} />
-      </div>
-      <button type="submit">Buscar</button>
-    </form>
+    <div className="container">
+      <form onSubmit={(e) => e.preventDefault()} className="buscarPaciente-form">
+        <h2>Buscar Paciente</h2>
+        <div className="input-group">
+          <label htmlFor="search">Buscar por cédula, nombre o apellido</label>
+          <input type="text" name="search" id="search" onChange={handleChange} value={formData.search} />
+        </div>
+        <div className="input-group">
+          <label htmlFor="patientsPerPage">Pacientes por página:</label>
+          <select
+            name="patientsPerPage"
+            id="patientsPerPage"
+            onChange={handlePatientsPerPageChange}
+            value={formData.patientsPerPage}
+          >
+            <option value="5">5</option>
+            <option value="10">10</option>
+            <option value="15">15</option>
+          </select>
+        </div>
+      </form>
 
-    <table>
-      <thead>
-        <tr>
-          <th>Nombre</th>
-          <th>Apellido</th>
-          <th>Cédula</th>
-          <th>Número de Teléfono</th>
-        </tr>
-      </thead>
-      <tbody>
-      {filteredPacientes.map(paciente => (
-        <tr key={paciente.ID_Paciente}>
-          <td>{paciente.nombre}</td>
-          <td>{paciente.apellido}</td>
-          <td>{paciente.cedula}</td>
-          <td>{paciente.telefono}</td>
-          <td><button onClick={() => handleViewChange(paciente)}>Ver Historia Clínica</button></td>
-        </tr>
-      ))}
-      </tbody>
-    </table>
-    
-    <button onClick={() => handleViewChange('principal')}>REGRESAR</button>
-    </>
+      <table>
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Apellido</th>
+            <th>Cédula</th>
+            <th>Número de Teléfono</th>
+          </tr>
+        </thead>
+        <tbody>
+          {currentPatients.map((paciente) => (
+            <tr key={paciente.ID_Paciente}>
+              <td>{paciente.Nombre}</td>
+              <td>{paciente.Apellido}</td>
+              <td>{paciente.Cedula}</td>
+              <td>{paciente.Telefono}</td>
+              <td>
+                <button onClick={() => handleViewChange('historiaClinica', paciente.ID_Paciente)}>
+                  Ver Historia Clínica
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <div className="pagination">
+        {currentPage > 1 && (
+          <button onClick={() => paginate(currentPage - 1)}>Anterior</button>
+        )}
+        {filteredPacientes.length > formData.patientsPerPage && (
+          <button onClick={() => paginate(currentPage + 1)}>Siguiente</button>
+        )}
+      </div>
+
+      <button onClick={() => handleViewChange('principal')}>REGRESAR</button>
+    </div>
   );
 }
