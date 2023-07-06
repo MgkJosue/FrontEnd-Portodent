ec594809fce1eec68373e187161532e8584e8ed1
MgkJosue <masderxd@gmail.com>
2023-07-06 08:14:30 -0500

    Validaciones de los campos del formulario

--- src/views/PacienteForm.js
+++ src/views/PacienteForm.js
@@ -1,9 +1,7 @@
-// PacienteForm.js
 import React, { useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import '../styles/PacienteForm.css';
-
 
 export default function PacienteForm() {
 
@@ -21,9 +19,31 @@ export default function PacienteForm() {
   const [showMessage, setShowMessage] = useState(false);
 
   const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
+    const { name, value } = e.target;
+    switch (name) {
+      case 'Nombre':
+      case 'Apellido':
+        if (/^[a-zA-Z\s]*$/.test(value)) {
+          setFormData({ ...formData, [name]: value });
+        }
+        break;
+      case 'Cedula':
+      case 'Telefono':
+        if (/^\d*$/.test(value)) {
+          setFormData({ ...formData, [name]: value });
+        }
+        break;
+      case 'FechaNacimiento':
+        const current = new Date().toISOString().split('T')[0];
+        if (value <= current) {
+          setFormData({ ...formData, [name]: value });
+        }
+        break;
+      default:
+        setFormData({ ...formData, [name]: value });
+        break;
+    }
   };
-
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -32,9 +52,7 @@ export default function PacienteForm() {
       console.log(response.data);
 
       setShowMessage(true);
-      setTimeout(() => {
-        setShowMessage(false);
-      }, 2000);
+      setTimeout(() => setShowMessage(false), 2000);
 
     } catch (error) {
       console.error(error);
@@ -44,57 +62,55 @@ export default function PacienteForm() {
   return (
     <div className="page-container">
       <div className={`form-container ${showMessage ? 'show-message' : ''}`}>
-      <div className="botones-container">
-        <Link to="/inicio-paciente">
-          <button>ATRAS</button>
-        </Link>
-        <Link to="/">
-          <button>INICIO</button>
-        </Link>
+        <div className="botones-container">
+          <Link to="/inicio-paciente">
+            <button>ATRAS</button>
+          </Link>
+          <Link to="/">
+            <button>INICIO</button>
+          </Link>
+        </div>
+        <form onSubmit={handleSubmit} className="paciente-form">
+          <h2>Registrar Paciente</h2>
+          <div className="input-group">
+            <label htmlFor="Nombre">Nombres</label>
+            <input type="text" name="Nombre" id="Nombre" onChange={handleChange} value={formData.Nombre} required />
+          </div>
+          <div className="input-group">
+            <label htmlFor="Apellido">Apellidos</label>
+            <input type="text" name="Apellido" id="Apellido" onChange={handleChange} value={formData.Apellido} required />
+          </div>
+          <div className="input-group">
+            <label htmlFor="Cedula">Cedula</label>
+            <input type="text" name="Cedula" id="Cedula" onChange={handleChange} value={formData.Cedula} required />
+          </div>
+          <label htmlFor="Sexo">Sexo</label>
+          <div className="form-control">
+            <input type="radio" name="Sexo" id="Masculino" onChange={handleChange} value="Masculino" required />
+            <label htmlFor="Masculino">Masculino</label>
+            <input type="radio" name="Sexo" id="Femenino" onChange={handleChange} value="Femenino" required />
+            <label htmlFor="Femenino">Femenino</label>
+          </div>
+          <br></br>
+          <div className="input-group">
+            <label htmlFor="FechaNacimiento">Fecha de Nacimiento</label>
+            <input type="date" name="FechaNacimiento" id="FechaNacimiento" onChange={handleChange} value={formData.FechaNacimiento} required />
+          </div>
+          <div className="input-group">
+            <label htmlFor="Direccion">Direccion</label>
+            <input type="text" name="Direccion" id="Direccion" onChange={handleChange} value={formData.Direccion} required />
+          </div>
+          <div className="input-group">
+            <label htmlFor="Telefono">Telefono</label>
+            <input type="tel" name="Telefono" id="Telefono" onChange={handleChange} value={formData.Telefono} required />
+          </div>
+          <div className="input-group">
+            <label htmlFor="Email">Email</label>
+            <input type="email" name="Email" id="Email" onChange={handleChange} value={formData.Email} required />
+          </div>
+          <button type="submit">Enviar</button>
+        </form>
       </div>
-    <form onSubmit={handleSubmit} className="paciente-form">
-      <h2>Registrar Paciente</h2>
-      <div className="input-group">
-        <label htmlFor="Nombre">Nombres</label>
-        <input type="text" name="Nombre" id="Nombre" onChange={handleChange} value={formData.Nombre} />
-      </div>
-      <div className="input-group">
-        <label htmlFor="Apellido">Apellidos</label>
-        <input type="text" name="Apellido" id="Apellido" onChange={handleChange} value={formData.Apellido} />
-      </div>
-      <div className="input-group">
-        <label htmlFor="Cedula">Cedula</label>
-        <input type="text" name="Cedula" id="Cedula" onChange={handleChange} value={formData.Cedula} />
-      </div>
-      <label htmlFor="Sexo">Sexo</label>
-      <div className="form-control ">
-        <input type="radio" name="Sexo" id="Masculino" onChange={handleChange} value="Masculino" />
-        <label htmlFor="Masculino">Masculino</label>
-        <input type="radio" name="Sexo" id="Femenino" onChange={handleChange} value="Femenino" />
-        <label htmlFor="Femenino">Femenino</label>
-      </div>
-      <br></br>
-      <div className="input-group">
-        <label htmlFor="FechaNacimiento">Fecha de Nacimiento</label>
-        <input type="date" name="FechaNacimiento" id="FechaNacimiento" onChange={handleChange} value={formData.FechaNacimiento} />
-      </div>
-      <div className="input-group">
-        <label htmlFor="Direccion">Direccion</label>
-        <input type="text" name="Direccion" id="Direccion" onChange={handleChange} value={formData.Direccion} />
-      </div>
-      <div className="input-group">
-        <label htmlFor="Telefono">Telefono</label>
-        <input type="text" name="Telefono" id="Telefono" onChange={handleChange} value={formData.Telefono} />
-      </div>
-      <div className="input-group">
-        <label htmlFor="Email">Email</label>
-        <input type="text" name="Email" id="Email" onChange={handleChange} value={formData.Email} />
-      </div>
-      <button type="submit">Enviar</button>
-
-    </form>
-
-    </div>
       {showMessage && (
         <div className="message-popup">
           <p><strong>SE GUARDÓ CORRECTAMENTE EL PACIENTE</strong></p>
@@ -103,4 +119,3 @@ export default function PacienteForm() {
     </div>
   ); 
 }
-
