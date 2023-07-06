1a552af2dd665242f3a6eb12aba220fb745a7581
Angel Velez <aivelez1@espe.edu.ec>
2023-07-06 13:04:41 -0500

    ver historia clinica

--- src/views/HistoriaClinica.js
+++ src/views/HistoriaClinica.js
@@ -1,42 +1,56 @@
-// HistoriaClinicaForm.js
-import React, { useState } from 'react';
+
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import '../styles/HistoriaClinica.css';
-import { Link } from 'react-router-dom';
 
+function HistorialClinico() {
+  const { pacienteId } = useParams();
+  const [nombrePaciente, setNombrePaciente] = useState('');
+  const [historialClinico, setHistorialClinico] = useState(null);
 
-export default function HistoriaClinicaForm() {
+  useEffect(() => {
+    const obtenerHistorialClinico = async () => {
+      try {
+        const response = await axios.get(`http://localhost:8000/historias_clinicas/${pacienteId}`);
+        const historiaClinica = response.data;
+        setHistorialClinico(historiaClinica);
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
+    obtenerHistorialClinico();
+  }, [pacienteId]);
 
-  const [formData, setFormData] = useState({
-    ID_Paciente: '',
-  });
+  useEffect(() => {
+    const obtenerNombrePaciente = async () => {
+      try {
+        const response = await axios.get(`http://localhost:8000/pacientes/${pacienteId}`);
+        const paciente = response.data;
+        setNombrePaciente(`${paciente.Nombre} ${paciente.Apellido}`);
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await axios.post('http://localhost:8000/historias_clinicas/', formData);
-      console.log(response.data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+    obtenerNombrePaciente();
+  }, [pacienteId]);
 
   return (
-    <><form onSubmit={handleSubmit} className="historia-form">
-      <h2>Crear Historia Clínica</h2>
-      <div className="input-group">
-        <label htmlFor="ID_Paciente">ID del Paciente</label>
-        <input type="text" name="ID_Paciente" id="ID_Paciente" onChange={handleChange} value={formData.ID_Paciente} />
-      </div>
-      <button type="submit">Enviar</button>
-
-    </form>
-    <Link to="/"><button>INICIO</button></Link>
-    </>
+    <div className="historial-container">
+      <h1 className="titulo-historial">HISTORIAL CLÍNICO DE {nombrePaciente}</h1>
+      {historialClinico ? (
+        <div className="contenido-historial">
+          {/* Renderizar los datos del historial clínico aquí */}
+          <p>ID de Historia Clínica: {historialClinico.ID_Historia}</p>
+          {/* ...otros campos del historial clínico */}
+        </div>
+      ) : (
+        <p className="carga-historial">Cargando historial clínico...</p>
+      )}
+    </div>
   );
 }
+
+export default HistorialClinico;
