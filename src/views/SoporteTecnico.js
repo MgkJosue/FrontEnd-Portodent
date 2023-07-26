9152349437d2187c580361939e78ee31bbbb323d
Angel Velez <aivelez1@espe.edu.ec>
2023-07-26 14:06:43 -0500

    soporte tecnico

--- src/views/SoporteTecnico.js
+++ src/views/SoporteTecnico.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import '../styles/SoporteTecnico.css'; // Asegúrate de tener el archivo de estilos SoporteTecnico.css correspondiente
 import { Link } from 'react-router-dom';
+import angel from '../images/perfilAngel.jpg';
+import josue from '../images/perfilJosue.jpg';
 
 export default function SoporteTecnico() {
 
 
   return (
-    <><div className="soporte-tecnico">
-      <h2>Soporte Técnico</h2>
-      <pre>NOMBRE: ANGEL IGNACIO VELEZ MORALES</pre>
-      <pre>TELEFONO: 0992187805</pre>
-      <pre>CORRE0: angelchino757@gmail.com </pre>
-      <pre>NOMBRE: JOSUE ISRAEL ALVAREZ GALARZA</pre>
-      <pre>TELEFONO: 0962737556</pre>
-      <pre>CORRE0: josue.alvarez9@hotmail.com</pre>
+    <><div className="container-form">
+      <div className='column'>
+        <img src={angel} alt="Imagen 1" className='angel'/>
+        <pre><strong>NOMBRE:</strong> ANGEL IGNACIO VELEZ MORALES</pre>
+        <pre><strong>TELEFONO:</strong> 0992187805</pre>
+        <pre><strong>CORRE0: </strong>angelchino757@gmail.com </pre>
+      </div>
+      <div className='column'>
+        <img src={josue} alt="Imagen 2" className='josue'/>
+        <pre><strong>NOMBRE:</strong> JOSUE ISRAEL ALVAREZ GALARZA</pre>
+        <pre><strong>TELEFONO:</strong> 0962737556</pre>
+        <pre><strong>CORRE0: </strong> josue.alvarez9@hotmail.com</pre>
+
+      </div> 
 
     </div>
     <Link to="/"><button>INICIO</button></Link>
